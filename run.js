cdba229794ad42b9f843f9bacf89e3e21c4e00c6
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2020-02-13 14:01:36 -0300

    Improving tests and logs

--- run.js
+++ run.js
@@ -1,9 +1,13 @@
 const bigbluebot = require('bigbluebot')
 
 let actions = async page => {
-  await bigbluebot.audio.modal.microphone(page)
+  await bigbluebot.audio.modal.listen(page)
   await bigbluebot.video.join(page)
+  await bigbluebot.audio.leave(page)
+  await bigbluebot.audio.modal.microphone(page)
+  await bigbluebot.audio.mute(page)
   await bigbluebot.chat.send(page)
+  await bigbluebot.audio.unmute(page)
 }
 
 bigbluebot.run(actions)
